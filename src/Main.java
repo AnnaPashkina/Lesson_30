93b78ec475ec4843bdf6896894140dac69595e51
AnnaPashkina <anya230489@gmail.com>
2022-12-19 23:30:09 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Scanner console = new Scanner(System.in);
+        while (true) {
+
+            String s_new = console.nextLine();
+            if (s_new.equals("exit")) {
+                break;
+
+            }
+        }*/
+        /*int i = 5;
+        do {
+            System.out.println(i);
+            i--;
+
+        }
+      while (i>0);*/
+     /* int l=5;
+      while (l>0)
+      {System.out.println(l);
+          l--;} */
+
+          int k=17;
+          do {System.out.println(" Я люблю выходные ");
+                  k--;}
+          while (k>0);
+
+      }
+
+
+    }
+
